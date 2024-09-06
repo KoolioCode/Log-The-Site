124b7f70c24ecb13ec00810c92d75f4509255e29
unknown <thekoolcraft@gmail.com>
2024-09-05 20:30:45 -0500

    second commit

---
+++ app.js
@@ -0,0 +1,36 @@
+let progress = document.getElementById("playback")
+let song = document.getElementById("song")
+let cplay = document.getElementById("cplay")
+
+song.onloadeddata =function(){
+    playback.max = song.duration;
+    playback.value = song.currentTime;
+}
+
+song.pause();
+
+function playPause(){
+    if(cplay.classList.contains("fa-pause")){
+        song.pause();
+        cplay.classList.remove("fa-pause");
+        cplay.classList.add("fa-play");
+    }
+    else{
+        song.play();
+        cplay.classList.add("fa-pause");
+        cplay.classList.remove("fa-play");
+    }
+}
+
+if(song.play()){
+    setInterval(() => {
+        progress.value = song.currentTime;
+    }, 500);
+}
+
+progress.onchange = function(){
+    song.play();
+    song.currentTime = progress.value;
+    cplay.classList.add("fa-pause");
+    cplay.classList.remove("fa-play");
+}
