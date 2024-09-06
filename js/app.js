99181429b8dcfc98a07cc7b812f581172dad347e
ProgWithLog <120297920+KoolioCode@users.noreply.github.com>
2024-09-05 21:48:24 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,14 +20,11 @@ function playPause(){
     }
 }
 
-if(song.play()){
     setInterval(() => {
         progress.value = song.currentTime;
     }, 500);
-}
 
 progress.onchange = function(){
-    song.play();
     song.currentTime = progress.value;
     cplay.classList.add("fa-pause");
     cplay.classList.remove("fa-play");
